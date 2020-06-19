f4db6867a78c6d53f3ef368341bdb0091f89914c
caroline-ctrl <duon.caroline@gmail.com>
2020-06-19 16:49:21 +0200

    first commit

---
+++ src/app/models/product.model.ts
@@ -0,0 +1,8 @@
+export class Product {
+    constructor(
+        public title: string,
+        public content: string,
+        public created_at: string,
+        public updated_at: string
+    ){}
+}
